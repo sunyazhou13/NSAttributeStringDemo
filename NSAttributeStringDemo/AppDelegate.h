569212b07bfcd591afb99a4b1a65cb4314a82897
sunyazhou <sunyazhou@kuaishou.com>
2018-06-15 10:48:59 +0800

    Initial Commit

---
+++ NSAttributeStringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSAttributeStringDemo
+//
+//  Created by sunyazhou on 2018/6/15.
+//  Copyright © 2018年 Kwai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSAttributeStringDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSAttributeStringDemo
+//
+//  Created by sunyazhou on 2018/6/15.
+//  Copyright © 2018年 Kwai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
